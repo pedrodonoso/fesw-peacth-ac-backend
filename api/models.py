68f8c53d0b29cc99a9a27211406db3948d470e3a
cdreyes <chrisrey.97@gmail.com>
2021-07-10 20:30:42 -0400

    [KI-0] Calculo dosis inicial
    - Setup inicial de la API.
    - CRUD Paciente.
    - Método custom para cálculo, por ahora de imc.

--- api/models.py
+++ api/models.py
@@ -1,17 +1,49 @@
 from djongo import models
 
+isMigrate = True
+
 # Create your models here.
+class Genetic(models.Model):
+    CYP2C9_2 = models.CharField(unique=True, max_length=5)
+    CYP2C9_3 = models.CharField(unique=True, max_length=5)
+    VKORC1 = models.CharField(unique=True, max_length=5)
+
+    class Meta:
+        abstract = isMigrate
+
+
+class Diagnosis(models.Model):
+    diagnosis1 = models.CharField(max_length=256)
+    diagnosis2 = models.CharField(max_length=256)
+    diagnosis3 = models.CharField(max_length=256)
+    diagnosis4 = models.CharField(max_length=256)
+
+    class Meta:
+        abstract = isMigrate
+
+    
+
 class Patient(models.Model):
     _id = models.ObjectIdField()
     code = models.CharField(unique=True, max_length=5)
     sex = models.CharField(max_length=1)
     bloodType = models.CharField(max_length=256)
     initialDate = models.CharField(max_length=256)
-    initialDosis = models.IntegerField()
+    initialDosis = models.IntegerField(default=0)
     initialINR = models.FloatField()
-    weeklyDosisInRange = models.IntegerField()
-    totalDays = models.IntegerField()
+    weeklyDosisInRange = models.IntegerField(default=0)
+    totalDays = models.IntegerField(default=0)
     weight = models.IntegerField()
     height = models.FloatField()
     imc = models.FloatField()
     age = models.IntegerField(default=0)
+
+    genetics = models.EmbeddedField(
+        model_container=Genetic
+    )
+
+    diagnosis = models.EmbeddedField(
+        model_container=Diagnosis
+    )
+
+    objects = models.DjongoManager()
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
+from rest_framework.serializers import Serializer
 from api import serializers
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from api.models import Patient
 from api.serializers import PatientSerializer
@@ -10,4 +12,26 @@ class PatientModelViewSet(viewsets.ModelViewSet):
     
     serializer_class = PatientSerializer
     queryset = Patient.objects.all()
+
+    @action(detail=True, methods=['post'])
+    def get_weekly_dosis(self, request, pk=None):
+        self.serializer_class = PatientSerializer
+
+        request_data = request.data
+
+        print(request_data['code'])
+
+        serializer = PatientSerializer(data=request_data)
+        print(serializer.is_valid())
+
+        if serializer.is_valid():
+            serializer.save()
+            imc = request_data["weight"]/(request_data["height"]**2)
+            response = {
+                'imc' : imc
+            }
+
+            return Response(response, status=status.HTTP_200_OK)
         
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
