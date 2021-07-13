97c4dcb6ac800adfc163d984a876e111b6dcf333
mnbarrio <mnbarrio@alumnos.inf.utfsm.cl>
2021-07-12 20:35:32 -0400

    [KI-7] Actualizar parametros del algoritmo manualmente - se utiliza los parametros ingresados para el cálculo de la dosis

--- PeacthAC/settings.py
+++ PeacthAC/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'peacth-ac',
-        #'CLIENT':{
-            #'host': 'mongodb+srv://kine001:6xG6jKScLdZYPGzh@cluster0.1l1z7.mongodb.net/peacth-ac?retryWrites=true&w=majority'
-        #}
+        'CLIENT':{
+            'host': 'mongodb+srv://kine001:6xG6jKScLdZYPGzh@cluster0.1l1z7.mongodb.net/peacth-ac?retryWrites=true&w=majority'
+        }
     }
 }
 
