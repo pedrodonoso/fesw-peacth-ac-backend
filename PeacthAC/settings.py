391d1cf9b7fb04d1292f9d8312c42a0c3e5a165e
cdreyes <chrisrey.97@gmail.com>
2021-10-25 15:32:16 -0300

    debug false

--- PeacthAC/settings.py
+++ PeacthAC/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b%^$51-x-+14!kuwtg1_a7!4vgpfb27w3a4b=e^k9kw)alqsbt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
